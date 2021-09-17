e5ce0cc8a4d77070141a118f4a853a495f679c8a
mcsandoval <mcsandoval@tamu.edu>
2021-09-16 23:46:55 -0500

    fresh start

--- PA1/BuddyAllocator.cpp
+++ PA1/BuddyAllocator.cpp
@@ -1,111 +1,27 @@
 #include "BuddyAllocator.h"
 #include <iostream>
-#include <math.h>
 using namespace std;
 
-BlockHeader* split (BlockHeader* b){
-  int bs = b->block_size;
-  b->block_size /= 2;
-  b->next =  nullptr;
-
-  BlockHeader* sh = (BlockHeader*) (char*)b + b->block_size;
-  BlockHeader* temp = new (sh) BlockHeader (b->block_size);
-  return sh;
-}
-
 BuddyAllocator::BuddyAllocator (int _basic_block_size, int _total_memory_length){
-  total_memory_size = _total_memory_length;
-  basic_block_size = _basic_block_size;
-	start = new char [total_memory_size];
-  int l = log2 (total_memory_size/basic_block_size);
-  for(int i  = 0; i < l;++i){
-    FreeList.push_back (LinkedList());
-  }
-  FreeList.push_back(LinkedList((BlockHeader*)start));
-  BlockHeader* h = new (start) BlockHeader (total_memory_size);
+	
 }
 
 BuddyAllocator::~BuddyAllocator (){
-	delete[] start;
-  // delete all the nodes from the list 
+	
 }
 
 char* BuddyAllocator::alloc(int _length) {
-  int x = _length + sizeof(BlockHeader);
-  int index =  ceil(log2 (ceil (x / (double) basic_block_size)));
-  //int blockSizeReturn = (1 << index) * basic_block_size;
-  if(FreeList[index].head != nullptr){ // found the block that we are looking for 
-    BlockHeader* b = FreeList[index].remove();
-    return  (char*) (b+1);
-  }
-  int indexCorrect = index;
-  for(; index < FreeList.size() + 1; index++){ // otherwise check the vector for a block of that size
-    if(FreeList[index].head){
-      break;
-    }
-  }
-
-  if(index >= FreeList.size()){
-    // no bigger block found then return a null
-    return nullptr;
-  }
-
-  // a bigger block was found
-  while (index > indexCorrect){
-    BlockHeader* b = FreeList[index].remove(); // get that block of memory
-    BlockHeader* shb = split (b); // split b into two blocks of equal size where b already has a pointer there
-    --index;
-    FreeList[index].insert(b); // insert b and shb to the space before our index since that will be half the size
-    FreeList[index].insert(shb);
-  }
-  return (char*) (FreeList[index].remove() + 1);  // remove the next pointer to the list
+  /* This preliminary implementation simply hands the call over the 
+     the C standard library! 
+     Of course this needs to be replaced by your implementation.
+  */
+  return new char [_length];
 }
 
 int BuddyAllocator::free(char* _a) {
-  // Given the address, frees up the memory region starting 
-  // at that address (i.e., consider it as free again for 
-  // future alloc() calls)
-
-  // get the start of the memory taken up by the block
-  // BlockHeader* s_a = (BlockHeader*) _a -1;
-  // s_a->next = nullptr;
-
-  BlockHeader* startingAddress = (BlockHeader*) ( _a - sizeof(BlockHeader));
-  BlockHeader* bh = startingAddress;
-
-  for(int i =0; i < FreeList.size();++i){
-    BlockHeader* b = FreeList[i].head;
-
-    while(b){
-      if(b == startingAddress){
-        return -1;
-      }
-      b = b->next;
-    }
-  }
-
-  // get the index for the list 
-  int x = startingAddress->block_size;
-  int index = (ceil(log2 (ceil ((double) x / basic_block_size))));
-
-  FreeList[index].insert(bh); // insert them into the list
-
-
-  // start merging the blocks of memory 
-  int newSize = bh->block_size;
-  bool merging = true;
-  while(merging){
-    BlockHeader* buddyAddress = getbuddy(startingAddress);
-    if(arebuddies(startingAddress, buddyAddress)){
-      startingAddress = merge(startingAddress,buddyAddress);
-      bh = (BlockHeader*) startingAddress;
-    }else {merging = false;}
-  }
-
+  /* Same here! */
+  delete _a;
   return 0;
-  // if alloc removes a free memory block from the FreeList, this should do the opposite and add that memory block to the LL
-  // delete[] _a;
-  // return 0;
 }
 
 void BuddyAllocator::printlist (){
@@ -128,4 +44,3 @@ void BuddyAllocator::printlist (){
     cout << count << endl;  
   }
 }
-
--- PA1/BuddyAllocator.h
+++ PA1/BuddyAllocator.h
@@ -2,9 +2,6 @@
 #define _BuddyAllocator_h_
 #include <iostream>
 #include <vector>
-#include <assert.h>
-#include <stdint.h>
-#include <math.h>
 using namespace std;
 typedef unsigned int uint;
 
@@ -15,69 +12,19 @@ public:
 	// think about what else should be included as member variables
 	int block_size;  // size of the block
 	BlockHeader* next; // pointer to the next block
-	bool isFree;
-
-	// constructor that takes in the size of a block 
-	BlockHeader (int size = 0){
-		block_size = size;
-		next = nullptr;
-	}
 };
 
 class LinkedList{
 	// this is a special linked list that is made out of BlockHeader s. 
 public:
 	BlockHeader* head;		// you need a head of the list
-	int ListSize;
 public:
-	// default constructor 
-	LinkedList (BlockHeader* h = nullptr){
-		head = h;
-	}
 	void insert (BlockHeader* b){	// adds a block to the list
-		// get the index of the block 
-		if(!head){
-			b->next = nullptr;
-		}else{
-			b->next = head;
-		}
 
-		head = b;
-		b->isFree = true;
-		++ListSize;
 	}
 
 	void remove (BlockHeader* b){  // removes a block from the list
-		BlockHeader* current = head;
-		while(current->next != b){
-			current = current->next;
-		}
-		
-		current->next = b->next;
-		b->isFree = false;
-		--ListSize;
-	}
 
-	BlockHeader* remove (){ // return the first item from the list
-		assert (head != nullptr);
-		BlockHeader* b = head;
-		head = head->next;
-		--ListSize;
-		b->isFree = false;
-		return b;
-	}
-
-	BlockHeader* getHead(){ return head; }
-
-	// this is a test function 
-	void print(){
-		BlockHeader* current = head;
-		std::cout << "[";
-		while(current){
-			std::cout << current << " " << current->block_size << "][";
-			current = current->next;
-		}
-		std::cout << "]" << std::endl;
 	}
 };
 
@@ -88,76 +35,35 @@ private:
 	vector<LinkedList> FreeList;
 	int basic_block_size;
 	int total_memory_size;
-	char* start; // this is where the memory starts
 
 private:
 	/* private function you are required to implement
 	 this will allow you and us to do unit test */
 	
-	BlockHeader* getbuddy (BlockHeader * addr){
-		// since the memory starts at address 0 xor the addr size
-		// to make this work for all cases we will make all the addreses equal to 0
-		// (block address – start) XOR (block size) + start
-		return (BlockHeader*) (((addr - (BlockHeader*) start) ^ addr->block_size) + start);
-	} 
+	BlockHeader* getbuddy (BlockHeader * addr); 
 	// given a block address, this function returns the address of its buddy 
 	
-	bool arebuddies (BlockHeader* block1, BlockHeader* block2){
-		return block2->isFree && block1->block_size == block2->block_size;
-	}
+	bool arebuddies (BlockHeader* block1, BlockHeader* block2);
 	// checks whether the two blocks are buddies are not
 
-	BlockHeader* merge (BlockHeader* block1, BlockHeader* block2){
-		// there a bad memeory access when dealing with the free function
-		// debug this functions 
-		// check the addresses 
-		int x = block1->block_size;
-		int index = (ceil(log2 (ceil ( x / (double) basic_block_size))));
-		
-		FreeList[index].remove(block1);
-		FreeList[index].remove(block2);
-
-		block1->block_size *= 2;
-		block1->next = nullptr;
-		
-		if (x >= total_memory_size){
-			return nullptr;
-		}
-
-		x = block1->block_size;
-		index = (ceil(log2 (ceil ( x / (double)basic_block_size))));
-		FreeList[index].insert(block1);
-		
-		return block1;
-	}
+	BlockHeader* merge (BlockHeader* block1, BlockHeader* block2);
 	// this function merges the two blocks returns the beginning address of the merged block
 	// note that either block1 can be to the left of block2, or the other way around
 
-	BlockHeader* split (BlockHeader* block){
-		block->block_size = block->block_size / 2;
-
-		BlockHeader * newblock = new BlockHeader();
-		newblock->block_size = block->block_size; // split == halfed
-		newblock->next = block->next;
-		block->next = newblock;
-
-		return newblock;
-	}
+	BlockHeader* split (BlockHeader* block);
 	// splits the given block by putting a new header halfway through the block
 	// also, the original header needs to be corrected
 
 
 public:
-	BuddyAllocator (int _basic_block_size, int _total_memory_length);
+	BuddyAllocator (int _basic_block_size, int _total_memory_length); 
 	/* This initializes the memory allocator and makes a portion of 
 	   ’_total_memory_length’ bytes available. The allocator uses a ’_basic_block_size’ as 
-	   its minimal unit of allocation. The function returns the amount of 
-	   memory made available to the allocator. If an error occurred, 
-	   it returns 0. 
+	   its minimal unit of allocation.  
 	*/ 
 
 	~BuddyAllocator(); 
-	/* Destructor that returns any allocated memory back to the operating system. 
+	/* Destructor that gives any allocated memory back to the operating system. 
 	   There should not be any memory leakage (i.e., memory staying allocated).
 	*/ 
 

034617a8dc9eec49f37c047422f1ed975aa2b52b
mcsandoval <mcsandoval@tamu.edu>
2021-11-01 22:50:34 -0500

    handle args

--- PA4/client.cpp
+++ PA4/client.cpp
@@ -5,8 +5,16 @@
 #include <sys/wait.h>
 using namespace std;
 
-void patient_thread_function(/*add necessary arguments*/){
+
+void patient_thread_function(int p, int t, int e, int n, vector<FIFORequestChannel *> openChannels){
     /* What will the patient threads do? */
+	//patient needs to be able to create a channel for its use
+	// requestChannels(openChannels); // open the channel 
+	// for(int i = 0; i < n; ++i){
+	// 	// make n data request to the channel
+
+	// }
+	// close the channel
 }
 
 void worker_thread_function(/*add necessary arguments*/){
@@ -19,19 +27,48 @@ void histogram_thread_function (/*add necessary arguments*/){
 		Functionality of the histogram threads	
     */
 }
-int main(int argc, char *argv[]){
 
+int main(int argc, char *argv[]){
+	vector<FIFORequestChannel*> openChannels;
 	int opt;
 	int p = 1;
 	double t = 0.0;
 	int e = 1;
 	string filename = "";
-	int b = 10; // size of bounded buffer, note: this is different from another variable buffercapacity/m
+	int b = 10; // size of bounded buffer, NOTE: this is different from another variable buffercapacity/m acceptable range [100, 1000000]
 	// take all the arguments first because some of these may go to the server
-	while ((opt = getopt(argc, argv, "f:")) != -1) {
+	int m = MAX_MESSAGE; // buffer capacity 
+	int n = 1;	// [1, 15k] number of data items
+	int w = 50; // [50,5k] worker threads 
+	int h = 1; // histogram threads
+	while ((opt = getopt(argc, argv, "f:p:t:e:b:m:n:w:h:")) != -1) {
 		switch (opt) {
 			case 'f':
 				filename = optarg;
+				break;
+			case 'p':
+				p =  atoi(optarg);
+				break;
+			case 't':
+				t = atof(optarg);
+				break;
+			case 'e':
+				e = atoi(optarg);
+				break;
+			case 'b':
+				b = atoi(optarg);
+				break;
+			case 'm':
+				m = atoi(optarg);
+				break;
+			case 'n':
+				n = atoi(optarg);
+				break;
+			case 'w':
+				w = atoi(optarg);
+				break;
+			case 'h':
+				h = atoi(optarg);
 				break;
 		}
 	}
@@ -46,7 +83,7 @@ int main(int argc, char *argv[]){
 			EXITONERROR ("Could not launch the server");
 		}
 	}
-	FIFORequestChannel chan ("control", FIFORequestChannel::CLIENT_SIDE);
+	FIFORequestChannel channel ("control", FIFORequestChannel::CLIENT_SIDE);
 	BoundedBuffer request_buffer(b);
 	HistogramCollection hc;
 
@@ -66,9 +103,13 @@ int main(int argc, char *argv[]){
     int usecs = (int)(end.tv_sec * 1e6 + end.tv_usec - start.tv_sec * 1e6 - start.tv_usec)%((int) 1e6);
     cout << "Took " << secs << " seconds and " << usecs << " micro seconds" << endl;
 	
-	// closing the channel    
-    Request q (QUIT_REQ_TYPE);
-    chan.cwrite (&q, sizeof (Request));
+	// close all the channels 
+	Request q (QUIT_REQ_TYPE);
+	channel.cwrite(&q, sizeof(Request));
+	// for(int i = 0; i < openChannels.size();++i){
+	// 	openChannels.back()->cwrite(&q, sizeof(Request));
+	// 	openChannels.pop_back();
+	// }
 	// client waiting for the server process, which is the child, to terminate
 	wait(0);
 	cout << "Client process exited" << endl;

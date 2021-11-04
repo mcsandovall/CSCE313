7418ae28e532c50a8a2417668f2bdb4b55219582
mcsandoval <mcsandoval@tamu.edu>
2021-11-04 00:24:34 -0500

    filerequest not hung, doesnt transfer well

--- PA4/client.cpp
+++ PA4/client.cpp
@@ -17,18 +17,24 @@ struct server_response{ // structure to hold the response data and patient numbe
 	server_response(double d, int pno) : data(d), p(pno) {}
 };
 
-void makeFile_request(int64 filesize,int buffersize, FileRequest * file_request, BoundedBuffer * requestBuffer){
+void makeFile_request(BoundedBuffer * request_buffer, string filename, int64 filelen, int buffer_size){
 	// this function makes all the file request and puts them in the request buffer
-	cout << "Start woker thread" << endl;
-	while(filesize > 0){
-		cout << filesize << endl;
-		file_request->length = min<int64>(filesize, (int64) buffersize);
-		vector<char> request((char*) file_request, (char*) file_request + sizeof(FileRequest));
-		requestBuffer->push(request);
-		filesize -= file_request->length;
-		file_request->offset += file_request->length;
+	FileRequest fm (0,0);
+	int len = sizeof (FileRequest) + filename.size()+1;
+	char buf2 [len];
+	memcpy (buf2, &fm, sizeof (FileRequest));
+	strcpy (buf2 + sizeof (FileRequest), filename.c_str());
+
+	FileRequest * fr = (FileRequest*) buf2;
+	int64 rem = filelen;
+	char buffer[buffer_size];
+	while(rem > 0){
+		fr->length = min<int64>(rem, (int64) buffer_size);
+		vector<char> rq ((char*) fr, (char*) fr + sizeof(FileRequest));
+		request_buffer->push(rq);
+		rem -= fr->length;
+		fr->offset += fr->length;
 	}
-	cout << "end worker thread" << endl;
 }
 
 FIFORequestChannel * createChannel(FIFORequestChannel * channel){
@@ -51,7 +57,6 @@ void patient_thread_function(int p, int n, BoundedBuffer * requestBuffer){
 		vector<char> request((char *)dt, (char *)dt + sizeof(DataRequest));
 		requestBuffer->push(request);
 		dt->seconds += 0.004;
-		//cout << i << endl;
 	}
 	//delete dt;
 }
@@ -68,7 +73,7 @@ void worker_thread_function(BoundedBuffer * requestBuffer,BoundedBuffer * respon
    ofstream ofs;
 
    if(filename != ""){
-	   ofs.open("recieved/"+ filename, ofstream::binary);
+	   ofs.open("received/"+ filename, ofstream::binary);
    }
 
    // cast a pointer of request type
@@ -98,7 +103,6 @@ void worker_thread_function(BoundedBuffer * requestBuffer,BoundedBuffer * respon
 
 		   // add it to the reponse buffer
 		   responseBuffers->push(data);
-		   //cout << "size of response buffer " << responseBuffers->size() << endl;
 
 		   //delete all the pointer to prevent memeory leak
 	   }
@@ -206,71 +210,90 @@ int main(int argc, char *argv[]){
 	struct timeval start, end;
     gettimeofday (&start, 0);
 
-	if(filename!= ""){ // this mean we have a file request
-		//query the size of the file
+	if(filename == ""){ // this handles the data request
+
+		//create patient threads
+		thread patients[p];
+		for(int i = 0; i < p; ++i){
+			patients[i] = thread (patient_thread_function, i+1, n, &request_buffer);
+			Histogram * h = new Histogram(10,-2.0,2.0);
+			hc.add(h);
+		}
+
+		// make worker channels
+		thread workers[w];
+		for(int i = 0; i < w; ++i){
+			FIFORequestChannel * n_channel = createChannel(channel);
+			workers[i] =  thread (worker_thread_function, &request_buffer, &response_buffer, n_channel, m, filename);
+		}
+
+		// make histogram threads 
+		thread hist[h];
+		for(int i = 0; i < h; ++i){
+			hist[i] = thread(histogram_thread_function, &response_buffer, &hc);
+		}
+
+		/* Join the threads */
+
+		for(int i = 0; i < p; ++i){
+			patients[i].join();
+		}
+
+		Request q (QUIT_REQ_TYPE);
+		for(int i = 0; i < w; ++i){
+			vector<char> d ((char*) &q, (char*)&q + sizeof(Request));
+			request_buffer.push(d);
+		}
+
+		for(int i = 0; i < w; ++i){
+			workers[i].join();
+		}
+
+		server_response sq (-1.0,-1);
+		for(int i = 0; i < h; ++i){
+			vector<char> d ((char*)&sq, (char*) &sq + sizeof(server_response));
+			response_buffer.push(d);
+		}
+
+		for(int i = 0; i < h; ++i){
+			hist[i].join();
+		}
+	}else{ // handle the filerequest
+		// get the filesize
 		FileRequest fm (0,0);
 		int len = sizeof (FileRequest) + filename.size()+1;
-		char buf [len];
-		memcpy (buf, &fm, sizeof (FileRequest));
-		strcpy (buf + sizeof (FileRequest), filename.c_str());
-		channel->cwrite (&buf, len);  
+		char buffer [len];
+		memcpy (buffer, &fm, sizeof (FileRequest));
+		strcpy (buffer + sizeof (FileRequest), filename.c_str());
+		channel->cwrite (&buffer, len);  
 		int64 filelen;
 		channel->cread (&filelen, sizeof(int64));
 		if (isValidResponse(&filelen)){
 			cout << "File length is: " << filelen << " bytes" << endl;
 		}
 
-		// make a thread that makes and puts all the request into the request buffer
-		thread file_worker(makeFile_request,filelen, m , (FileRequest*) buf, &request_buffer);
+		// create file request thread
+		thread fr_thread(makeFile_request, &request_buffer, filename, filelen, m);
 
-		file_worker.join();
-	}
-    /* Start all threads here */
-	thread patients[p];
-	for(int i = 0; i < p;++i){
-		patients[i] = thread(patient_thread_function,i+1,n,&request_buffer);
-		// create the number of histograms
-		Histogram * h = new Histogram(10,-2.0,2.0);
-		hc.add(h);
-	}
+		// make worker channels
+		thread workers[w];
+		for(int i = 0; i < w; ++i){
+			FIFORequestChannel * n_channel = createChannel(channel);
+			workers[i] =  thread (worker_thread_function, &request_buffer, &response_buffer, n_channel, m, filename);
+		}
 
-	thread workers[w];
-	for(int i = 0; i < w; ++i){
-		FIFORequestChannel * n_channel = createChannel(channel);
-		workers[i] = thread(worker_thread_function,&request_buffer, &response_buffer, n_channel, m, filename);
-	}
+		// join the threads
+		fr_thread.join();
 
-	thread hist[h];
-	for(int i = 0; i < h;++i){
-		hist[i] = thread(histogram_thread_function,&response_buffer,&hc);
-	}
-
-	/* Join all threads here */
-	for (int i = 0; i < p;++i){
-		patients[i].join();
-	}
-
-	// kill the worker thread
-	for(int i  = 0; i < w; ++i){
 		Request q (QUIT_REQ_TYPE);
-		vector<char> rq ((char*) &q, (char*) &q  + sizeof(Request));
-		request_buffer.push(rq);
-	}
+		for(int i = 0; i < w; ++i){
+			vector<char> d ((char*) &q, (char*)&q + sizeof(Request));
+			request_buffer.push(d);
+		}
 
-	cout << "Request buffer " << request_buffer.size() << endl;
-	for(int i = 0; i < w; ++i){
-		workers[i].join();
-	}
-
-	// kill the histogram threads
-	for(int i = 0; i < h; ++i){
-		server_response qr(-1.0,-1);
-		vector<char> q ((char*) &qr, (char*) &qr + sizeof(server_response));
-		response_buffer.push(q);
-	}
-
-	for(int i = 0; i < h;++i){
-		hist[i].join();
+		for(int i = 0; i < w; ++i){
+			workers[i].join();
+		}
 	}
 	
     gettimeofday (&end, 0);
